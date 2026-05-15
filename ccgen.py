6848dc15c66684ef5e49786dd9eca92be038286b
HACKER999990 <114234215+HACKER999990@users.noreply.github.com>
2026-05-15 01:14:56 -0700

    ccgen.py

--- ccgen.py
+++ ccgen.py
@@ -155,4 +155,4 @@ if __name__ == "__main__":
     app.add_handler(CommandHandler("gen", gen))
     
     print("✅ Bot is running...")
-    app.run_polling(allowed_updates=Update.ALL_TYPES)
+    app.run_polling(allowed_updates=Update.ALL_TYPES, drop_pending_updates=True)
